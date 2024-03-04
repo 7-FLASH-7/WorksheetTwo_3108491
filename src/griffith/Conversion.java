290f50463911fc6543b9eb8c6e3c69eea6e15812
Harsh777 <harshpatil02072004@gmail.com>
2024-03-04 19:54:57 +0000

    I have created stub methods and completd the part one

--- src/griffith/Conversion.java
+++ src/griffith/Conversion.java
@@ -1,5 +1,34 @@
 package griffith;
 
 public class Conversion {
-
+	
+		//Stub method to convert celcius To Fahrenheit 
+	public double celciusToFahrenheit(double celcius) {
+			return 0.0;
+	}
+	
+	//Stub method to convert fahrenheit To Celcius
+	public double fahrenheitToCelcius(double fahrenhei) {
+			return 0.0;
+	}
+	
+	//Stub method to convert miles To Kilometer
+	public double milesToKilometer(double miles) {
+		return 0.0;
+	}
+	
+	//Stub method to convert kilometer To Miles
+	public double kilometerToMiles(double kilometer) {
+		return 0.0;
+	}
+	
+	//Stub method to convert uppercase To Lowercase
+	public String uppercaseToLowercase(String uppercase) {
+		return null;
+	}
+	
+	//Stub method to convert lowercase To Uppercase
+	public String lowercaseToUppercase(String lowercase) {	
+		return null;
+	}
 }
--- src/griffith/ConversionTest.java
+++ src/griffith/ConversionTest.java
@@ -1,5 +1,31 @@
 package griffith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions.*;
 
 public class ConversionTest {
-
+	
+	@Test
+	public void testCelciusToFahrenheit() {
+		}
+	
+	@Test
+	public void testFahrenheitToCelcius() {
+		}
+	
+	@Test
+	public void testMilesToKilometer() {
+		}
+	
+	@Test
+	public void testKilometerToMiles(){
+		}
+	
+	@Test
+	public void testUppercaseToLowercase() {	
+		}
+	
+	@Test
+	public void testLowercaseToUppercase() {
+		
+	}
 }
